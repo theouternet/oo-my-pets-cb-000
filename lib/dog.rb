c34e567733107303e70514d28a386a5d1138a983
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-30 02:12:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-  # code goes here
+  # code class Cat
+
+attr_accessor :mood, :owner
+attr_reader :name
+
+@@all = []
+
+def initialize(name, owner)
+  @name = name
+  @owner = owner
+  @mood = "nervous"
+@@all << self
+end
+
+def self.all
+  @@all 
+end
+
+
+end
+ here
 end
