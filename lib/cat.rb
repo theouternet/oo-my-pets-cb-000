f1ad4583b2fd9e8374e9fc63f9f1ed01f11e4169
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-30 01:57:50 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,7 +3,7 @@ class Cat
 attr_accessor :mood
 attr_reader :name, :owner
 
-def initialize(name, @owner)
+def initialize(name, @owner.name)
   @name = name
   
   
@@ -12,3 +12,19 @@ end
 
 
 end
+
+
+  before do
+    @owner = Owner.new("Hermione")
+    @cat = Cat.new("Crookshanks", @owner)
+  end
+
+  after do
+    Owner.all.clear
+  end
+
+  context 'Instance methods' do
+    it "initializes with a name and an Owner" do
+      expect(@cat.name).to eq("Crookshanks")
+      expect(@cat.owner.name).to eq("Hermione")
+    end
