555e82ef456e50081f5866dc0eb63d44e632d728
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-30 02:01:38 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -13,18 +13,3 @@ end
 
 end
 
-
-  before do
-    @owner = Owner.new("Hermione")
-    @cat = Cat.new("Crookshanks", @owner)
-  end
-
-  after do
-    Owner.all.clear
-  end
-
-  context 'Instance methods' do
-    it "initializes with a name and an Owner" do
-      expect(@cat.name).to eq("Crookshanks")
-      expect(@cat.owner.name).to eq("Hermione")
-    end
