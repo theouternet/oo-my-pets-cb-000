6ba64f982b8f9fcb19565ee97666f44a02ff8ff0
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-30 02:11:22 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,11 +3,17 @@ class Cat
 attr_accessor :mood, :owner
 attr_reader :name
 
+@@all = []
+
 def initialize(name, owner)
   @name = name
   @owner = owner
   @mood = "nervous"
 
+end
+
+def self.all
+  @@all 
 end
 
 
